1997e3b66457783dd88e69cc8453a624c55fa411
williamcioffi <william.cioffi@gmail.com>
2022-05-28 18:56:34 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # contactsheet_flashcards
+
+expects a key file csv named imagekey.csv
+expects a directory called images/ with png tiff or jpeg/jpg.
+
+imagekey.csv should have minimum three columns
+filename with the name not the path of the file
+id
+name
+
+id and name get pasted to form the label (answer)
+
+requires
+library(tiff)
+library(jpeg)
+library(png)
+library(shiny)
---
+++ flashcards.r
@@ -0,0 +1,77 @@
+library(shiny)
+library(tiff)
+library(png)
+library(jpeg)
+
+# load files
+key <- read.table("imagekey.csv", header = TRUE, sep = ',', stringsAsFactors = FALSE)
+imagepaths <- file.path("images", key$filename)
+nimages <- length(imagepaths)
+curdis <- 1
+
+key$label <- paste(key$id, key$name)
+
+stdplot <- function(...) {
+  plot(
+    0, 0, type = 'n', axes = FALSE, 
+    xlim = c(0, 1), ylim = c(0, 1),
+    xlab = "", ylab = "",
+    ...
+  )
+}
+
+loadimage <- function(imagepath) {
+  extension <- sapply(strsplit(imagepaths[1], split = "[.]"), function(x) x[length(x)])
+  img <- NULL
+  
+  if(grepl("jp*g", extension)) {
+    img <- jpeg::readJPEG(imagepath)
+  } else if(grepl("tif*", extension)) {
+    img <- tiff::readTIFF(imagepath)
+  } else if(grepl("png", extension)) {
+    img <- png::readPNG(imagepath)
+  } else {
+    warning(paste("warning unable to open", imagepath))
+  }
+  
+  img
+}
+
+displayimage <- function(i) {
+  img <- loadimage(i)
+  r <- as.raster(img[,,1:3])
+  stdplot(asp = 1)
+  rasterImage(r, 0, 0, 1, 1)
+}
+
+ui <- fluidPage(
+  actionButton('go', 'go'), 
+  actionButton('tell', 'tell!'),
+  textOutput("answer"),
+  mainPanel(
+    plotOutput("flashcard", width = 800, height = 800),
+  )
+)
+
+server <- function(input, output, session) {
+  curdis <<- sample(nimages, 1)
+  output$answer <- renderText({"guess!"})
+  output$flashcard <- renderPlot({
+    displayimage(imagepaths[curdis])
+  })
+
+  observeEvent(input$go, {
+    curdis <<- sample(nimages, 1)
+    output$answer <- renderText({"guess!"})
+    output$flashcard <- renderPlot({
+      displayimage(imagepaths[curdis])
+    })
+  })
+  
+  observeEvent(input$tell, {
+    output$answer <- renderText({key$label[curdis]})
+  })
+  
+}
+
+shinyApp(ui, server)
